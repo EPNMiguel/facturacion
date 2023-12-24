92db0cd0d9f85095c49ac6c0e4cbcc2568c40778
EPNMiguel <miguel_gaibor_mg@hotmail.com>
2023-12-24 10:56:45 -0500

    fix: metodo Login
    Cambio el metodo GET a POST para Login

--- src/main/java/com/qph/facturacion/controller/UsuarioController.java
+++ src/main/java/com/qph/facturacion/controller/UsuarioController.java
@@ -3,18 +3,16 @@ package com.qph.facturacion.controller;
 import com.qph.facturacion.entity.Usuario;
 import com.qph.facturacion.service.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController()
 @RequestMapping(path = "api/v1/usuario")
 public class UsuarioController {
     @Autowired
-    private  UsuarioService usuarioService ;
-    @GetMapping("/{user}/{pass}")
-    public boolean ValidaEntrada(@PathVariable("user")String user, @PathVariable("pass") String pass){
-        return usuarioService.ValidaEntrada(user, pass );
+    private UsuarioService usuarioService;
+
+    @PostMapping("/valida")
+    public boolean ValidaEntrada(@RequestBody Usuario user) {
+        return usuarioService.ValidaEntrada(user.getUser(), user.getPassword());
     }
 }
